59d3c247e44459e061cd38c02e434ab5b3ec2f30
Ari Kristiansen <arkri9000@hotmail.com>
2014-02-19 12:05:47 +0100

    ghadsfhg

--- SysUt2014Gr1/gitTest/gitTest.cs
+++ SysUt2014Gr1/gitTest/gitTest.cs
@@ -13,6 +13,10 @@ namespace gitTest
             Console.Write(String.Format("->{1,10}<-", "GitHubTest maddafakka!"));
             Console.Write(String.Format("funker this sh!t?"));
             Console.Write("yes.... it does work :P");
+            for (int i = 0; i > -1; i++)
+            {
+                Console.Write("Evig loop! oh shit");
+            }
         }
     }
 }
