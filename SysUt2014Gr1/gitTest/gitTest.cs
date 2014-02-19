6122d69d7412d58f7c834b48d7b4922fbda7c7f4
Ari Kristiansen <arkri9000@hotmail.com>
2014-02-19 11:39:30 +0100

    GitHub Test

---
+++ SysUt2014Gr1/gitTest/gitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitTest
+{
+    class gitTest
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(String.Format("->{1,10}<-", "GitHubTest maddafakka!"));
+        }
+    }
+}
