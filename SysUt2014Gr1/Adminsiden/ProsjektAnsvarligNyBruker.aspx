947de771e7c48a776c6b27b14d6544298f00a9f2
KristianAlm <kristian.alm83@gmail.com>
2014-03-10 15:38:08 +0100

    legg til bruker fra prosjektansvarlig

---
+++ SysUt2014Gr1/Adminsiden/ProsjektAnsvarligNyBruker.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ProsjektAnsvarligNyBruker.aspx.cs" Inherits="Adminsiden.ProsjektAnsvarligNyBruker" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Legg til nye brukere</title>
+    <link rel="Stylesheet" type="text/css" href="css/ProsjektAnsvarligNyBruker.css" />
+</head>
+<body>
+
+    <div id="adminLogo">
+        <asp:Image runat="server" ImageUrl="Resources/MorildData.png" AlternateText="Morild Data BA" />
+    </div>
+
+    <form id="ProsjektAnsvarligNyBruker" runat="server">
+    
+    <div>
+
+        <div id="overskriftInsert">
+                <asp:Label runat="server">Legg til nye brukere</asp:Label>
+            </div>
+
+        <asp:GridView ID="GridViewProsjektAnsvarligInsert" runat="server" AutoGenerateColumns="False" ShowHeaderWhenEmpty="True" AutoGenerateEditButton="True"
+                OnRowCancelingEdit="GridViewProsjektAnsvarligInsert_RowCancelingEdit" OnRowEditing="GridViewProsjektAnsvarligInsert_RowEditing" OnRowUpdating="GridViewProsjektAnsvarligInsert_RowUpdating"
+                CssClass="gridView" AlternatingRowStyle-CssClass="alt">
+
+                <AlternatingRowStyle CssClass="alt" />
+
+                <Columns>
+                    <asp:BoundField DataField="surname" HeaderText="Etternavn"/>
+                    <asp:BoundField DataField="firstname" HeaderText="Fornavn"/>
+                    <asp:BoundField DataField="username" HeaderText="Brukernavn"/>
+                    <asp:BoundField DataField="password" HeaderText="Passord"/>
+                    <asp:BoundField DataField="phone" HeaderText="Telefon"/>
+                    <asp:BoundField DataField="mail" HeaderText="Mail"/>
+
+                    <asp:TemplateField HeaderText="Team">
+                        <EditItemTemplate>
+                            <asp:DropDownList ID="dropDownTeam" DataSource="<%# DropDownBoxTeam() %>" 
+                                DataTextField="teamName" DataValueField="teamID" runat="server"></asp:DropDownList>
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+
+                    <asp:BoundField DataField="groupName" HeaderText="Brukertype" ReadOnly="True" ItemStyle-Width="8%" ItemStyle-HorizontalAlign="Center" />
+                </Columns>
+         </asp:GridView>
+
+        <div id="beskjedTilBruker">
+                <asp:Label ID="beskjed" runat="server"></asp:Label>
+            </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ SysUt2014Gr1/Adminsiden/ProsjektAnsvarligNyBruker.aspx.cs
@@ -0,0 +1,92 @@
+﻿using Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Adminsiden
+{
+    public partial class ProsjektAnsvarligNyBruker : System.Web.UI.Page
+    {
+        private DBConnect db;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            db = new DBConnect();
+
+            if (!Page.IsPostBack)
+            {
+                GridViewInsertEmpty();
+            }
+        }
+
+        protected DataTable DropDownBoxTeam()
+        {
+            string query = "SELECT * FROM Team";
+            DataTable table = new DataTable();
+            table = db.AdminGetAllUsers(query);
+            table.Rows.InsertAt(table.NewRow(), 0);
+
+            return table;
+        }
+
+        private void GridViewInsertEmpty()
+        {
+            string query = "SELECT userID, surname, firstname, username, password, phone, mail, teamName, groupName FROM User, Team, UserGroup WHERE userID = 0 AND User.teamID = Team.teamID AND User.groupID = UserGroup.groupID";
+            DataTable dt = db.AdminGetAllUsers(query);
+            dt.Columns["groupName"].DefaultValue = "Bruker";
+            dt.Rows.Add(dt.NewRow());
+            GridViewProsjektAnsvarligInsert.DataSource = dt;
+            GridViewProsjektAnsvarligInsert.DataBind();
+        }
+
+        protected void GridViewProsjektAnsvarligInsert_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = GridViewProsjektAnsvarligInsert.Rows[e.RowIndex];
+
+            try
+            {
+                string surname = e.NewValues["surname"].ToString();
+                string firstname = e.NewValues["firstname"].ToString();
+                string username = e.NewValues["username"].ToString();
+                string password = e.NewValues["password"].ToString();
+                string phone = e.NewValues["phone"].ToString();
+                string mail = e.NewValues["mail"].ToString();
+                DropDownList team = (DropDownList)row.FindControl("dropDownTeam");
+
+                int teamID = Convert.ToInt32(team.SelectedValue);
+
+                string query = String.Format("INSERT INTO User (surname, firstname, password, username, phone, mail, teamID, groupID, aktiv) VALUES('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', {7}, {8})",
+                surname, firstname, password, username, phone, mail, teamID, "1", "1");
+
+                db.InsertDeleteUpdate(query);
+                GridViewProsjektAnsvarligInsert.EditIndex = -1;
+                
+                GridViewInsertEmpty();
+
+                beskjed.Text = "Ny brukeren er lagt til";
+            }
+            catch (Exception exception)
+            {
+                string error = exception.Message;
+                beskjed.Text = "Det oppsto en feil: " + error;
+            }
+        }
+
+        protected void GridViewProsjektAnsvarligInsert_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridViewProsjektAnsvarligInsert.EditIndex = e.NewEditIndex;
+            GridViewInsertEmpty();
+        }
+
+        protected void GridViewProsjektAnsvarligInsert_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridViewProsjektAnsvarligInsert.EditIndex = -1;
+            GridViewInsertEmpty();
+        }
+    }
+}
