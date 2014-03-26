07d27a67d5636f10841b0806dc4249d260051b33
henningfredriksen <rhymfaxe2@hotmail.com>
2014-03-26 12:48:58 +0100

    Lagt til opprett fase

---
+++ SysUt2014Gr1/Adminsiden/ProsjektAnsvarligNyFase.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ProsjektAnsvarligNyFase.aspx.cs" Inherits="Adminsiden.ProsjektAnsvarligNyFase" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+    <style type="text/css">
+        #Text2 {
+            margin-bottom: 1px;
+        }
+        #TextArea1 {
+            height: 99px;
+            width: 264px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <p style="height: 75px; margin-left: 40px">
+            Fasenavn&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="tbPhasename" runat="server" MaxLength="40" Width="298px"></asp:TextBox>
+            <br />
+            <br />
+            Dato&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="tbDateFrom" runat="server" TextMode="Date"></asp:TextBox>
+&nbsp;til
+            <asp:TextBox ID="tbDateTo" runat="server" TextMode="Date"></asp:TextBox>
+<%-- 
+            <asp:RangeValidator ID="RangeValidator1"
+                ControlToValidate="tbDateTo"
+                MinimumValue="2005-01-01"
+                MaximumValue="2005-12-31"
+                Type="Date"
+                EnableClientScript="false"
+                Text="The date must be between 2005-01-01 and 2005-12-31!"
+                runat="server" />
+--%>
+        </p>
+        <p style="margin-left: 40px">
+            Beskrivelse&nbsp;&nbsp;
+            <asp:TextBox ID="tbDescription" runat="server" Height="107px" TextMode="MultiLine" Width="300px"></asp:TextBox>
+        </p>
+    
+    </div>
+        <p style="margin-left: 40px">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnSubmit" runat="server" Text="Gjennomfør endringer" Width="212px" OnClick="btnSubmit_Click" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ SysUt2014Gr1/Adminsiden/ProsjektAnsvarligNyFase.aspx.cs
@@ -0,0 +1,59 @@
+﻿using Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Adminsiden
+{
+    public partial class ProsjektAnsvarligNyFase : System.Web.UI.Page
+    {
+        private DBConnect db = new DBConnect();
+
+        private DateTime phaseDateFrom = new DateTime();
+        private DateTime phaseDateTo = new DateTime();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void WriteData(String _pName, DateTime _pDateFrom, DateTime _pDateTo, String _pDesc)
+        {
+            String query = "INSERT INTO Fase (phaseName, phaseFromDate, phaseToDate, phaseDescription) VALUES('" + _pName + "', '" + _pDateFrom.ToString("yyyy-MM-dd") + "', '" + _pDateTo.ToString("yyyy-MM-dd") + "', '" + _pDesc + "')";
+            db.InsertDeleteUpdate(query);
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            String dateFrom = tbDateFrom.Text;
+            phaseDateFrom = Convert.ToDateTime(dateFrom);
+
+            String dateTo = tbDateTo.Text;
+            phaseDateTo = Convert.ToDateTime(dateTo);
+
+            // trenger sjekk for om phaseDateTo < phaseDateFrom (så du ikke kan sette sluttdato tidligere enn startdato)
+
+            // sjekker om tbDescription er tom, og varierer input til WriteData() ettersom phaseDescription skal kunne være NULL
+            if (tbDescription.Text == "")
+                WriteData(tbPhasename.Text, phaseDateFrom, phaseDateTo, "");
+
+            else
+                WriteData(tbPhasename.Text, phaseDateFrom, phaseDateTo, tbDescription.Text);
+        }
+        /*
+                protected void Disabled_DayRender(object sender, DayRenderEventArgs e)
+                {
+                    if (phaseDateTo < phaseDateFrom) //specify your condition on yours days
+                    {
+                        e.Day.IsSelectable = false;
+                        e.Cell.ForeColor = System.Drawing.Color.Gray;
+                    }
+                }
+         */
+    }
+}
