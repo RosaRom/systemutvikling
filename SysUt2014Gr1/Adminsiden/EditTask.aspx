f2606abebf84876cf612fa217b659bee4b90dc0f
tmfredriksen <tord.fredriksen@gmail.com>
2014-04-09 12:19:36 +0200

    nye funksjoner i EditTask. Måtte bytte navn og fjerne et ord i visTaskDetaljer som ødela for clean og build

---
+++ SysUt2014Gr1/Adminsiden/EditTask.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EditTask.aspx.cs" Inherits="Adminsiden.EditTask" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 1032px;
+            width: 1543px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label3" runat="server" Text="Tasknavn"></asp:Label>
+        <br />
+        <asp:TextBox ID="tbTaskName" runat="server"></asp:TextBox>
+    
+        <br />
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Legg til bruker"></asp:Label>
+    
+    </div>
+        <asp:DropDownList ID="ddlAddUser" runat="server">
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Allokert tid"></asp:Label>
+        <p>
+            <asp:TextBox ID="tbAllocatedTime" runat="server"></asp:TextBox>
+        </p>
+        <asp:Label ID="Label2" runat="server" Text="Prioritet"></asp:Label>
+        <p>
+        <asp:TextBox ID="tbPriority" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label6" runat="server" Text="Beskrivelse"></asp:Label>
+        </p>
+        <p>
+            <asp:TextBox ID="tbDescription" runat="server"></asp:TextBox>
+        </p>
+        <br />
+        <asp:Label ID="Label7" runat="server" Text="Fase"></asp:Label>
+        <br />
+        <asp:TextBox ID="tbPhase" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label8" runat="server" Text="State"></asp:Label>
+        <br />
+        <asp:TextBox ID="tbState" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label9" runat="server" Text="BacklogID"></asp:Label>
+        <br />
+        <asp:TextBox ID="tbBacklog" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Gjør avhengig av"></asp:Label>
+        <asp:DropDownList ID="ddlDependency" runat="server">
+        </asp:DropDownList>
+        <asp:Button ID="btnDependency" runat="server" Text="OK" />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="btnSave" runat="server" OnClick="btnSave_Click" Text="Lagre endringer" />
+        <br />
+    </form>
+</body>
+</html>
--- SysUt2014Gr1/Adminsiden/EditTask.aspx.cs
+++ SysUt2014Gr1/Adminsiden/EditTask.aspx.cs
@@ -12,9 +12,11 @@ namespace Adminsiden
     public partial class EditTask : System.Web.UI.Page
     {
         private DBConnect db;
-        private string query, userQuery, saveQuery;
+        private string query, userQuery, saveQuery, taskQuery;
         private DataTable dataTable = new DataTable();
         private DataTable userTable = new DataTable();
+        private DataTable taskTable = new DataTable();
+
         private int taskID = 12;
 
         protected void Page_Load(object sender, EventArgs e)
@@ -34,11 +36,16 @@ namespace Adminsiden
 
             userQuery = "SELECT * FROM User WHERE aktiv = 1";
             userTable = db.getAll(userQuery);
+            taskQuery = "SELECT * FROM Task";
+            taskTable = db.getAll(taskQuery);
             try
             {
                 tbTaskName.Text = dataTable.Rows[0]["taskName"].ToString();
                 tbDescription.Text = dataTable.Rows[0]["description"].ToString();
                 tbAllocatedTime.Text = dataTable.Rows[0]["hoursAllocated"].ToString();
+                tbPhase.Text = dataTable.Rows[0]["phaseID"].ToString();
+                tbState.Text = dataTable.Rows[0]["state"].ToString();
+                tbBacklog.Text = dataTable.Rows[0]["productBacklogID"].ToString();
                 //endring her medfører rapportinnsending //NB!! Må legges til
                 tbPriority.Text = dataTable.Rows[0]["priority"].ToString();
 
@@ -46,6 +53,11 @@ namespace Adminsiden
                 ddlAddUser.DataValueField = "userID";
                 ddlAddUser.DataSource = userTable;
                 ddlAddUser.DataBind();
+
+                ddlDependency.DataTextField = "taskName";
+                ddlDependency.DataValueField = "taskID";
+                ddlDependency.DataSource = taskTable;
+                ddlDependency.DataBind();
 
             }
             catch (IndexOutOfRangeException e)
@@ -58,10 +70,12 @@ namespace Adminsiden
 
         protected void btnSave_Click(object sender, EventArgs e)
         {
-           saveQuery = String.Format("UPDATE Task SET taskName = '{0}', description = '{1}', priority = {2}, hoursAllocated = {3} WHERE taskID = {4}",
-               tbTaskName.Text, tbDescription.Text, tbPriority.Text, tbAllocatedTime.Text, taskID);
+           saveQuery = String.Format("UPDATE Task SET taskName = '{0}', description = '{1}', priority = {2}, state = {3}, hoursAllocated = {4}, phaseID ={5}, productBacklogID = {6} WHERE taskID = {7}",
+               tbTaskName.Text, tbDescription.Text, tbPriority.Text, tbState.Text, tbAllocatedTime.Text, tbPhase.Text, tbBacklog.Text, taskID);
             db.InsertDeleteUpdate(saveQuery);
         }
+
+    
         /*
         private void GetTasks()
         {
