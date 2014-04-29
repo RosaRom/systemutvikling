5f3440f2824b244c6b23e757f41744bdbf5d3235
henningfredriksen <rhymfaxe2@hotmail.com>
2014-04-29 23:18:39 +0200

    vis / rediger hovedtask-forms

---
+++ SysUt2014Gr1/Adminsiden/PAEditHovedtask.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+// det er ikke gjort stort med utseendet, siden formen er så enkel og vi trenger et uniformt utseende uansett
+
+namespace Adminsiden
+{
+    public partial class PAEditHovedtask : System.Web.UI.Page
+    {
+        private int taskCategoryID = 1; // hardkodet, må byttes ut
+
+        private DBConnect db = new DBConnect();
+        private DataTable dt = new DataTable();
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                PopulateFields();
+            }
+        }
+
+        public void PopulateFields()
+        {
+            string query = String.Format("SELECT * FROM TaskCategory WHERE taskCategoryID = {0}", taskCategoryID);
+            dt = db.getAll(query);
+            tbTaskCategoryName.Text = dt.Rows[0]["taskCategoryName"].ToString();
+            taTaskCategoryDesc.Text = dt.Rows[0]["taskCategoryDescription"].ToString();            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (taTaskCategoryDesc.Text != "" && tbTaskCategoryName.Text != "")
+            {
+                String desc = taTaskCategoryDesc.Text;
+                String name = tbTaskCategoryName.Text;
+                string query = String.Format("UPDATE TaskCategory SET TaskCategoryDescription = \"{1}\", TaskCategoryName =\"{2}\" WHERE TaskCategoryID = {0}", taskCategoryID, desc, name);
+                db.InsertDeleteUpdate(query);
+            }
+            else
+                lbError.Text = "Feltene kan ikke være tomme.";
+        }
+    }
+}
---
+++ SysUt2014Gr1/Adminsiden/PAVisHovedtask.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PAVisHovedtask.aspx.cs" Inherits="Adminsiden.PAVisHovedtask" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="lbTaskCategoryName" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:Label ID="lbTaskCategoryDesc" runat="server" Text="Beskrivelse"></asp:Label><asp:TextBox id="taTaskCategoryDesc" TextMode="multiline" Columns="50" Rows="5" runat="server" />
+    
+    </div>
+    </form>
+</body>
+</html>
