ec0837fc019a7681d4ff32bb422ef8b7ddab8f32
henningfredriksen <rhymfaxe2@hotmail.com>
2014-05-01 20:33:51 +0200

    godkjenning av ekstra tid

---
+++ SysUt2014Gr1/Adminsiden/PAGodkjennEkstraTid.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Adminsiden
+{
+    public partial class PAGodkjennEkstraTid : System.Web.UI.Page
+    {
+        DBConnect db = new DBConnect();
+        DataTable dt = new DataTable();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void stuff()
+        {
+            string query = String.Format("SELECT * FROM Task WHERE extraHours != null");
+            dt = db.getAll(query);
+
+
+        }
+    }
+}
