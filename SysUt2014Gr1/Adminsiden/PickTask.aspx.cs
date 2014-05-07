4e0fb4b136174fd8a544b55921b68d6a13ab0eca
tmfredriksen <tord.fredriksen@gmail.com>
2014-05-07 16:08:03 +0200

    Kan endre rett task.

---
+++ SysUt2014Gr1/Adminsiden/PickTask.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Adminsiden
+{
+    public partial class PickTask : System.Web.UI.Page
+    {
+
+        DBConnect db = new DBConnect();
+        DataTable dt = new DataTable();
+        DataTable dtTest = new DataTable();
+
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            String userLoggedIn = (String)Session["userLoggedIn"];
+
+            if (userLoggedIn == "teamMember")
+                this.MasterPageFile = "~/Masterpages/Bruker.Master";
+
+            else if (userLoggedIn == "teamLeader")
+                this.MasterPageFile = "~/Masterpages/Teamleder.Master";
+
+            else if (userLoggedIn == "admin")
+                this.MasterPageFile = "~/Masterpages/Admin.Master";
+
+            else
+                this.MasterPageFile = "~/Masterpages/Prosjektansvarlig.Master";
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string session = (string)Session["userLoggedIn"];
+
+            if (session == "projectManager")
+            {
+                Populate();
+            }
+            else
+            {
+                Server.Transfer("Login.aspx", true);
+            }
+        }
+
+        public void Populate()
+        {
+            int projectID = Convert.ToInt16(Session["projectID"]);
+
+                string query = "SELECT taskID, taskCategoryID, taskName, description FROM Task";
+
+                dt = db.getAll(query);
+                ViewState["table"] = dt;
+
+                gvTaskList.DataSource = dt;
+                gvTaskList.DataBind();
+        }
+
+        protected void gvTaskList_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int index = Convert.ToInt32(e.CommandArgument.ToString());
+
+            //kjører når "Endre" blir trykket for en task
+            if (e.CommandName == "endre")
+            {
+                int taskID = Convert.ToInt32(dt.Rows[index]["taskID"].ToString());
+
+                Session["taskID"] = taskID;
+                Server.Transfer("EditTask.aspx", true);
+              
+            }
+        }
+    }
+}
