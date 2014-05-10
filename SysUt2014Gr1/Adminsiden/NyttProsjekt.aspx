743f8d6d24f0c40630682e70ba0ddbb6b402b351
henningfredriksen <rhymfaxe2@hotmail.com>
2014-05-10 23:02:55 +0200

    NyttProsjekt lagt til

---
+++ SysUt2014Gr1/Adminsiden/NyttProsjekt.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NyttProsjekt.aspx.cs" Inherits="Adminsiden.NyttProsjekt" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="lbProjectName" runat="server" Text="Prosjektnavn"></asp:Label>
+&nbsp;<asp:TextBox ID="tbProjectName" runat="server" Width="284px"></asp:TextBox>
+        <br />
+        <asp:Label ID="lbSelectStartDate" runat="server" Text="Startdato"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="tbStartDate" runat="server" TextMode="Date" AutoPostBack="True" OnTextChanged="tbStartDate_TextChanged"></asp:TextBox>
+        &nbsp;<asp:Label ID="lbEndDate" runat="server" Text="Tildato"></asp:Label>
+&nbsp;<asp:TextBox ID="tbEndDate" runat="server" ReadOnly="True" TextMode="Date"></asp:TextBox>
+        <br />
+        <asp:Label ID="lbSelectNumberOfPhases" runat="server" Text="Antall faser" ></asp:Label>
+&nbsp;<asp:TextBox ID="tbSelectNumberOfPhases" runat="server" TextMode="Number" Width="50px" AutoPostBack="True" OnTextChanged="tbSelectNumberOfPhases_TextChanged" onkeypress="return functionx(event)"></asp:TextBox>
+&nbsp;<asp:Label ID="lbSelectLengthOfPhase" runat="server" Text="Fasene skal være"></asp:Label>
+&nbsp;<asp:TextBox ID="tbSelectNumberOfDaysPerPhase" runat="server" TextMode="Number" Width="50px" AutoPostBack="True" OnTextChanged="tbSelectNumberOfDaysPerPhase_TextChanged" onkeypress="return functionx(event)"></asp:TextBox>
+&nbsp;<asp:Label ID="lbDays" runat="server" Text="dager lange."></asp:Label>
+        <br />
+        <asp:Label ID="lbDescription" runat="server" Text="Beskrivelse"></asp:Label>
+&nbsp;<asp:TextBox ID="tbDescription" runat="server" Rows="5" TextMode="MultiLine" Width="300px"></asp:TextBox>
+        <br />
+        <asp:Label ID="lbMakeSubproject" runat="server" Text="Underprosjekt av "></asp:Label>
+&nbsp;<asp:DropDownList ID="ddlSubProject" runat="server" AppendDataBoundItems="True" Height="20px" Width="200px">
+        </asp:DropDownList>
+        <br />
+        <asp:Label ID="lbAddTeam" runat="server" Text="Legg team til prosjektet"></asp:Label>
+&nbsp;<asp:DropDownList ID="ddlTeam" runat="server" AppendDataBoundItems="True" Width="200px">
+        </asp:DropDownList>
+        <br />
+        <asp:Label ID="lbAddTaskCategory" runat="server" Text="Legg til hovedtask"></asp:Label>
+&nbsp;<asp:DropDownList ID="ddlTaskCategory" runat="server" AppendDataBoundItems="True" Width="200px">
+        </asp:DropDownList>
+&nbsp;<asp:Button ID="btnAddTaskCategory" runat="server" OnClick="btnAddTaskCategory_Click" style="margin-bottom: 0px" Text="Legg til hovedtask" />
+        <asp:Label ID="lbTaskCategoryError" runat="server"></asp:Label>
+        <br />
+&nbsp;<asp:ListBox ID="taskCategoryList" runat="server" AppendDataBoundItems="True" Rows="5" Width="200px"></asp:ListBox>
+        <br />
+        <br />
+        <asp:Button ID="btnCreateProject" runat="server" OnClick="btnCreateProject_Click" Text="Legg til prosjekt" />
+        <asp:Label ID="lbError" runat="server" ForeColor="Red"></asp:Label>
+        <br />
+    
+    </div>
+    </form>
+     <script type = "text/javascript">
+         function functionx(evt) {
+             if (evt.charCode > 31 && (evt.charCode < 48 || evt.charCode > 57)) {
+                 alert("Allow Only Numbers");                 
+                 return false;
+             }
+         }
+     </script>
+</body>
+</html>
---
+++ SysUt2014Gr1/Adminsiden/NyttProsjekt.aspx.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Data;
+using System.Collections;
+
+namespace Adminsiden
+{
+    public partial class NyttProsjekt : System.Web.UI.Page
+    {
+        private DBConnect db = new DBConnect();        
+        private DateTime startDate = new DateTime();
+        private DateTime endDate = new DateTime();
+        private bool datesOK = false;
+        private List<String> taskCategoryIDs = new List<String>(); // lagrer ingenting
+        private List<String> taskCategories = new List<String>();
+
+        private int projectID;
+        private int taskCategoriCounter = 0;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                GetTeams();
+                GetParentProjects();
+                GetTaskCategories();
+            }
+        }
+
+        private void GetTeams()
+        {
+            string query = "SELECT * FROM Team";
+            ddlTeam.DataSource = db.getAll(query);
+            ddlTeam.DataTextField = "teamName";
+            ddlTeam.DataValueField = "teamID";
+            ddlTeam.Items.Insert(0, new ListItem("<Velg team>", "0")); //OBS! AppendDataBoundItems="true" i asp-kodene om dette skal funke!
+            ddlTeam.DataBind();
+        }
+
+        private void GetParentProjects()
+        {            
+            string query = "SELECT projectName, projectID FROM Project WHERE parentProjectID = 0 AND projectState = 1";
+            ddlSubProject.DataSource = db.getAll(query);
+            ddlSubProject.DataTextField = "projectName";
+            ddlSubProject.DataValueField = "projectID";
+            ddlSubProject.Items.Insert(0, new ListItem("<Velg hovedprosjekt>", "0")); //OBS! AppendDataBoundItems="true" i asp-kodene om dette skal funke!
+            ddlSubProject.DataBind();
+        }
+
+        private void GetTaskCategories()
+        {
+            string query = "SELECT * FROM TaskCategory";
+            ddlTaskCategory.DataSource = db.getAll(query);
+            ddlTaskCategory.DataTextField = "taskCategoryName";
+            ddlTaskCategory.DataValueField = "taskCategoryID";
+            ddlTaskCategory.Items.Insert(0, new ListItem("<Velg hovedtask>", "0")); //OBS! AppendDataBoundItems="true" i asp-kodene om dette skal funke!
+            ddlTaskCategory.DataBind();
+        }
+        
+        // event som oppdaterer endDate-boksen når startdato , antall faser og faselengde er fylt ut
+        private void UpdateProjectEndDate()
+        {
+            if (tbSelectNumberOfPhases.Text != "" && tbSelectNumberOfDaysPerPhase.Text != "" && tbStartDate.Text != "")
+            {
+                    startDate = Convert.ToDateTime(tbStartDate.Text);
+                    endDate = startDate.AddDays(Convert.ToInt32(tbSelectNumberOfPhases.Text) * Convert.ToInt32(tbSelectNumberOfDaysPerPhase.Text));
+                    tbEndDate.Text = endDate.ToString("yyyy-MM-dd");
+                    datesOK = true;
+            }
+        }
+
+        private void PopulateTaskCategoryListBox(List<String> _list)
+        {
+            taskCategoryList.DataSource = _list;
+            taskCategoryList.DataBind();
+        }
+
+        // legger til hovedtask
+        // && (tbTaskCategoryPhase.Text != "" && Convert.ToInt32(tbTaskCategoryPhase.Text.ToString()) <= Convert.ToInt32(tbSelectNumberOfPhases.Text.ToString()))
+        protected void btnAddTaskCategory_Click(object sender, EventArgs e)
+        {
+            if (ddlTaskCategory.SelectedIndex != 0)
+            {                
+//                taskCat.Add(Convert.ToInt32(ddlTaskCategory.SelectedValue));
+                DataTable dt = new DataTable();
+                string taskCategoryListQuery = string.Format("SELECT taskCategoryID, taskCategoryName, productBacklogID FROM TaskCategory WHERE taskCategoryID = {0}", ddlTaskCategory.SelectedValue);
+                dt = db.getAll(taskCategoryListQuery);
+
+//                if (taskCategoriIDs.Contains(dt.Rows[0]["taskCategoryID"].ToString()) != true)
+//                {
+                    taskCategories.Add(dt.Rows[0]["productBacklogID"] + " " + dt.Rows[0]["taskCategoryName"]);
+                    PopulateTaskCategoryListBox(taskCategories);
+                    taskCategoryIDs.Add(dt.Rows[0]["taskCategoryID"] + " ");
+                    taskCategoriCounter += 1;
+                    lbTaskCategoryError.Text = "";
+/*                }
+                else
+                {
+                    lbTaskCategoryError.ForeColor = Color.Red;
+                    lbTaskCategoryError.Text = "Du har allerede lagt til denne hovedtasken";
+                }
+*/
+            }
+        }
+
+        // lagrer prosjektet i db
+        protected void btnCreateProject_Click(object sender, EventArgs e)
+        {
+            string query;
+            
+            if (ddlSubProject.SelectedIndex != 0)
+            {
+                query = string.Format("INSERT INTO Project (projectName, projectDescription, projectState, parentProjectID, teamID, latestProject)" +
+                    " VALUES ('{0}', '{1}', {2}, {3}, {4}, {5})", tbProjectName.Text, tbDescription.Text, 0, ddlSubProject.SelectedValue, ddlTeam.SelectedValue, 1);
+            }
+            else
+            {
+                query = string.Format("INSERT INTO Project (projectName, projectDescription, projectState, teamID, latestProject) VALUES ('{0}', '{1}', {2}, {3}, {4})", tbProjectName.Text, tbDescription.Text, 0, ddlTeam.SelectedValue, 1);
+            }
+            
+            if (tbProjectName.Text != "" && datesOK == true && ddlTeam.SelectedIndex != 0)
+            {
+                DataTable dt = new DataTable();
+                db.InsertDeleteUpdate(query);
+                string latestProjectQuery = String.Format("SELECT projectID FROM Project WHERE latestProject = 1");
+                dt = db.getAll(latestProjectQuery);
+                projectID = Convert.ToInt32(dt.Rows[0]["projectID"]);
+
+                string resetLatestProjectQuery = string.Format("UPDATE Project SET latestProject = 0  WHERE projectID = {0}", projectID);
+                db.InsertDeleteUpdate(resetLatestProjectQuery);
+
+                // kjører metodene nedenfor
+                AddPhases();
+
+                if (taskCategoriCounter > 0)
+                {
+                    AddTaskCategories();
+                }
+
+                lbError.ForeColor = Color.Black;
+                lbError.Text = "Prosjekt lagt til, men er inaktivt. Du aktiverer det under 'Rediger Prosjekt'.";
+            }
+            else
+            {
+                lbError.ForeColor = Color.Red;
+                lbError.Text = "Prosjektet må ha navn, startdato, antall faser og hvor lange fasene skal være. I tillegg må et team velges.";
+            }
+        }
+
+        // legger til fasene i db
+        private void AddPhases()
+        {
+            for (int i = 0; i < Convert.ToInt32(tbSelectNumberOfPhases.Text); i++)
+            {
+                int fase = (i)+1;
+                DateTime phaseStartDate = new DateTime();
+                phaseStartDate = startDate.AddDays(Convert.ToInt32(tbSelectNumberOfDaysPerPhase.Text) * (fase));
+                
+                DateTime phaseEndDate = new DateTime();
+                phaseEndDate = phaseStartDate.AddDays(Convert.ToInt32(tbSelectNumberOfDaysPerPhase.Text) - 1);
+
+                string phaseQuery = string.Format("INSERT INTO Fase (phaseName, phaseDescription, phaseFromDate, phaseToDate, projectID)" +
+                    " VALUES ('{0}', '{1}', '{2}', '{3}', {4})", "Fase " + (fase - 1), "Beskrivelse for sprint " + (fase - 1), phaseStartDate.ToString("yyyy-MM-dd"), phaseEndDate.ToString("yyyy-MM-dd"), projectID);
+                db.InsertDeleteUpdate(phaseQuery);
+            }
+        }
+
+        // legger til hovedtasks i db ( FUNKER IKKE FORELØPIG, pga taskCategoriIDs[i]-lista )
+        private void AddTaskCategories()
+        {
+            
+            for (int i = 0; i < taskCategoriCounter; i++)
+            {
+                string taskCategoriesQuery = string.Format("UPDATE TaskCategory SET projectID = {0}" +
+                    " WHERE taskCategoryID = {1}", projectID, taskCategoryIDs[i]);
+
+                db.InsertDeleteUpdate(taskCategoriesQuery);
+            }
+        }
+
+        protected void tbSelectNumberOfPhases_TextChanged(object sender, EventArgs e)
+        {
+            UpdateProjectEndDate();
+        }
+
+        protected void tbSelectNumberOfDaysPerPhase_TextChanged(object sender, EventArgs e)
+        {
+            UpdateProjectEndDate();
+        }
+
+        protected void tbStartDate_TextChanged(object sender, EventArgs e)
+        {
+            UpdateProjectEndDate();
+        }
+
+    }
+}
